b38c7ec59f696cf1089e933efe971ff166689745
Thiago Machado Pedro <94803123+ThiagoMachadoPedro@users.noreply.github.com>
2024-03-26 17:06:46 -0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,29 +14,6 @@ class UserSeeder extends Seeder
           public function run(): void
           {
 
-                    User::create([
-                              'name' => 'Ricardo',
-                              'imageUser' => 'https://th.bing.com/th/id/R.a5a06254d71e4d18cb6179ed0a8af870?rik=KXOBGhSoNXDVuQ&pid=ImgRaw&r=0',
-                              'nick' => 'ursinho',
-                              'email' => 'xx@gmail.com',
-                              'password' => bcrypt('123456789'),
-                    ]);
-
-                    User::create([
-                              'name' => 'Alex',
-                              'imageUser' => 'https://th.bing.com/th/id/R.a5a06254d71e4d18cb6179ed0a8af870?rik=KXOBGhSoNXDVuQ&pid=ImgRaw&r=0',
-                              'nick' => 'Vicius',
-                              'email' => 'cc@gmail.com',
-                              'password' => bcrypt('123456789'),
-                    ]);
-
-                    User::create([
-                              'name' => 'Fulano',
-                              'imageUser' => 'https://th.bing.com/th/id/R.a5a06254d71e4d18cb6179ed0a8af870?rik=KXOBGhSoNXDVuQ&pid=ImgRaw&r=0',
-                              'nick' => 'Fulano Nick',
-                              'email' => 'vv@gmail.com',
-                              'password' => bcrypt('123456789'),
-                    ]);
 
                     User::create([
                               'name' => 'Thiago',
